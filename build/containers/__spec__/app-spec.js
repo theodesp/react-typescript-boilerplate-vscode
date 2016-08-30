c7eba9bad6de586e70197a740775b025a1fa4ddf
thdespou <tdespoudis@quotix.com>
2016-08-30 15:16:53 +0000

    Remove build

--- build/containers/__spec__/app-spec.js
+++
@@ -1,7 +0,0 @@
-import { expect } from "chai";
-describe("app", () => {
-    it("creates new app", () => {
-        expect(1).to.be.ok;
-    });
-});
-//# sourceMappingURL=app-spec.js.map
--- build/containers/app.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-export default class App extends React.Component {
-    render() {
-        return (React.createElement("div", null, "Hello world"));
-    }
-}
-//# sourceMappingURL=app.js.map
